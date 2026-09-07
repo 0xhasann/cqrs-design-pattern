7ae69d8ee6688e0ec9cc385d2faaa7799829ba0b
Mahboob Hasan <hasanmahboob059@gmail.com>
2026-09-07 23:58:34 +0530

    kafka configs

--- product-query-service/src/main/java/com/javatechie/dto/ProductEvent.java
+++ product-query-service/src/main/java/com/javatechie/dto/ProductEvent.java
@@ -5,11 +5,33 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class ProductEvent {
 
     private String eventType;
+
+    public String getEventType() {
+        return eventType;
+    }
+
+    public void setEventType(String eventType) {
+        this.eventType = eventType;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
     private Product product;
+
+    public ProductEvent(String eventType, Product product) {
+        this.eventType = eventType;
+        this.product = product;
+    }
+
+    public ProductEvent() {
+    }
 }
